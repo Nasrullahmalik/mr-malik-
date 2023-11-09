486fd4f0addf0aad9f4018c0e1e066f1a44d5549
Nasrullahmalik <130395171+Nasrullahmalik@users.noreply.github.com>
2023-11-09 10:04:56 +0500

    Add files via upload

--- plugins/malik-openai.js
+++ plugins/malik-openai.js
@@ -2,14 +2,10 @@ import fetch from "node-fetch"
 import { generateWAMessageFromContent } from "@adiwajshing/baileys"
 import fs from 'fs'
 import { Configuration, OpenAIApi } from 'openai'
-const configuration = new Configuration({ organization: global.org, apiKey: global.openAiapi});
+const configuration = new Configuration({ organization: '5pCOr5MZ9XFPHUWPP78TBKdx', apiKey: 'ThZzt3Ox4l786j0g0kaFT3BlbkFJ9BJKJ0xXVHvDwtb0Jz4M'});
 const openai = new OpenAIApi(configuration);
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 try {
-  if (global.openAiapi === "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")
-            return m.reply('Please add your _API_ key in *config.js* file')
-    if (global.org === "org-5pCOr5MZ9XFPHUWPP78TBKdx")
-            return m.reply('Please add your _ORG_ key in *config.js* file') 
             if (!text) return m.reply(`Chat with AI.\n\nExample:\n${usedPrefix}${command} write a poem for those who copy or steal others codes `);
             await conn.reply(m.chat, `Please wait, I\'m processing your request.\nIt may take 1-2 min so be paitent.\n\n${fig}`, m)
 
