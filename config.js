8391c2f20235e6f5eff3e09807ee0539104e77af
Nasrullahmalik <130395171+Nasrullahmalik@users.noreply.github.com>
2023-11-12 12:20:05 +0500

    Add files via upload

--- config.js
+++ config.js
@@ -1,85 +1,258 @@
+// - - THANKS TO - -
+// • GOD
+// • ImYanXiao
+// • GURU
+// • TeamOldUser
+// • Nurutomo
+// • Ikratos
+// • TECHNICAL-NAVEED
+// • Bochilgaming
+// • Rominaru
+// • Kannachann
+// • FahriAdison
+// • The.Sad.Boy01
+// • Rlxfly
+// • Rasel comel
+// • Xtreshe (Beban)
+// • Dll
+
 import { watchFile, unwatchFile } from 'fs'
 import chalk from 'chalk'
 import { fileURLToPath } from 'url'
-import fs from 'fs'
-import fetch from 'node-fetch'
-import axios from 'axios'
+import moment from 'moment-timezone'
 
 
+
+/*==============USER-INFO==============*/
 global.owner = [
-  ['923494757886', 'Nasrullah', true],
-  [''],
-  [''] 
-] //Number of owners
+  ['923124442554', 'mr-malik', true],
+  ['923124442554', 'Mr-malik', true] 
+] 
 
-global.mods = ['923494757886','923494757886'] 
-global.prems = ['923494757886', '923494757886', '923494757886']
+global.mods = ['923124442554',''] //Don't replace this,If you want to add someone add any dev
+global.prems = ['923124442554','923494757886']
+global.nomorbot = '923124442554' //Change it to your own
+global.nomorown = '923124442554' //Change it to your own
+global.namebot = 'mr-malik' //change it to your own
+global.nameown = 'Nasrullah' // change it to your own
+
+//*============= ==============*/
+
+
+
+/*============== CHATGPT ==============*/
+global.openAiapi = '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'   // Get you Api-key from 'https://platform.openai.com/account/api-keys'
+global.org = 'org-hE49SehUQEPAGJAy20yr4Om9'   // Get you Org-Key from 'https://platform.openai.com/account/org-settings' 
+//*============= ==============*/
+
+
+
+/*============= TAIMU =============*/
+let wibh = moment.tz('Asia/Dhaka').format('HH')
+    let wibm = moment.tz('Asia/Dhaka').format('mm')
+    let wibs = moment.tz('Asia/Dhaka').format('ss')
+    let wktuwib = `${wibh} H ${wibm} M ${wibs} S`
+    
+    let d = new Date(new Date + 3600000)
+    let locale = 'id'
+    // d.getTimeZoneOffset()
+    // Offset -420 is 18.00
+    // Offset    0 is  0.00
+    // Offset  420 is  7.00
+    let weton = ['Pahing', 'Pon', 'Wage', 'Kliwon', 'Legi'][Math.floor(d / 84600000) % 5]
+    let week = d.toLocaleDateString(locale, { weekday: 'long' })
+    let date = d.toLocaleDateString(locale, {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric'
+    })
+      
+    
+/*============== SOCIAL ==============*/
+
+// if you don't have it, type "-" or leave it alone so that there are credits :v
+global.sig = '-' //Link Instagrammu
+global.sgh = '-' //Link Githubmu
+global.sgc = '-'
+global.sdc = '-' //Fill it out using the Discord link, if there is one, just leave it
+global.snh = '-' //Hmmm, This is up to you to fill in :v
+
+/*============== PAYMENT ==============*/
+global.pdana = '-' //Nomor Danamu
+global.ppulsa = '-' //Nomor SimCard Yang Kamu Pake
+global.psaweria = '-' //Link Saweriamu Kalo Nggada Ketik - aja
+
+
+
+
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
-global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
-global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
-global.keysxteam = keysxteammm[Math.floor(keysxteammm.length * Math.random())]
-global.keysneoxrrr = ['5VC9rvNx', 'cfALv5']
-global.keysneoxr = keysneoxrrr[Math.floor(keysneoxrrr.length * Math.random())]
 global.lolkeysapi = ['BrunoSobrino']
-global.openAiapi = 'ThZzt3Ox4l786j0g0kaFT3BlbkFJ9BJKJ0xXVHvDwtb0Jz4M'   // Get you Api-key from 'https://platform.openai.com/account/api-keys'
-global.org = '5pCOr5MZ9XFPHUWPP78TBKdx'
+global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
+global.lolkey = 'apikeymu'
+global.xkey = 'APIKEYMU'
+global.cricket_URL = 'https://m.cricbuzz.com/cricket-commentary/75602/nz-vs-sl-41st-match-icc-cricket-world-cup-2023' //paste new live match url here
+/*============== API ==============*/
 global.APIs = { // API Prefix
   // name: 'https://website'
-  ApiEmpire: 'https://api-brunosobrino.zipponodes.xyz',
-  xteam: 'https://api.xteam.xyz', 
-  dzx: 'https://api.dhamzxploit.my.id',
+  nrtm: 'https://nurutomo.herokuapp.com',
+  xteam: 'https://api.xteam.xyz',
+  zahir: 'https://zahirr-web.herokuapp.com',
   lol: 'https://api.lolhuman.xyz',
-  violetics: 'https://violetics.pw',
-  neoxr: 'https://api.neoxr.my.id',
-  zenzapis: 'https://zenzapis.xyz',
-  akuari: 'https://api.akuari.my.id',
-  akuari2: 'https://apimu.my.id',
-  nrtm: 'https://fg-nrtm.ddns.net',
-  bg: 'http://bochil.ddns.net',
-  fgmods: 'https://api-fgmods.ddns.net'
+  ana: 'https://anabotofc.herokuapp.com/',
+  adiisus: 'https://adiixyzapi.herokuapp.com',
+  violetics : 'https://violetics.pw',
+  zenz: 'https://zenzapi.xyz',
+  males : 'https://malesin.xyz',
+  fgmods: 'https://api-fgmods.ddns.net',
 }
 global.APIKeys = { // APIKey Here
   // 'https://website': 'apikey'
-  'https://api.xteam.xyz': 'd90a9e986e18778b',
-  'https://api.lolhuman.xyz': '85faf717d0545d14074659ad',
-  'https://api.neoxr.my.id': `${keysneoxr}`,	
+  'https://api.xteam.xyz': 'APIKEYMU',
+  'https://anabotofc.herokuapp.com/': 'AnaBot',
+  'https://api.lolhuman.xyz': 'Apikeymu',
+  'https://zahirr-web.herokuapp.com': 'zahirgans',
+  'https://zenzapi.xyz': '01ABEB1E11',
   'https://violetics.pw': 'beta',
-  'https://zenzapis.xyz': `${keysxxx}`, 
-  'https://api-fgmods.ddns.net': 'fg-dylux'
+  'https://api-fgmods.ddns.net': 'fg-dylux'
+}
+// The word APIKEY is filled with your own apikey, by buying it on the website
+
+/*============== WATERMARK ==============*/
+global.wm = '「 Mr-Malik 」' //Main Watermark
+global.wm2 = '꒷︶꒷꒥꒷ ‧₊˚ ꒰ฅ˘Malik-Experts- ฅ ꒱ ‧₊˚꒷︶꒷꒥꒷'
+global.wm3 = 'Nasrullah'
+global.footerTXT = `Mr-malik • : +923124442554`
+global.fig = ``
+global.botdate = `🛑 Date: ${week} ${date}`
+global.bottime = `🧑‍💻 Time : ${wktuwib}`
+global.titlebot = '🎋 ┊  mr-malik'
+global.author = global.wm
+global.logo = 'https://i.ibb.co/jLnr3K9/20231025-195821.jpg'
+
+
+
+/*============== LOGO ==============*/
+global.thumb = 'https://i.ibb.co/jLnr3K9/20231025-195821.jpg' //Main Thumbnail
+global.thumb2 = 'https://i.ibb.co/jLnr3K9/20231025-195821.jpg'
+global.thumbbc = 'https://i.ibb.co/jLnr3K9/20231025-195821.jpg' //For broadcast
+global.giflogo = 'https://telegra.ph/file/a46ab7fa39338b1f54d5a.mp4'
+global.thumblvlup = 'https://i.ibb.co/jLnr3K9/20231025-195821.jpg'
+global.hwaifu = ['https://i.ibb.co/jLnr3K9/20231025-195821.jpg']
+
+/*============== FlamingText ===========*/
+global.flaaa = [
+'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text=', 
+'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&script=water-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextColor=%23000&shadowGlowColor=%23000&backgroundColor=%23000&text=',
+'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=crafts-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&text=',
+'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=amped-logo&doScale=true&scaleWidth=800&scaleHeight=500&text=',
+'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text=',
+'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&fillColor1Color=%23f2aa4c&fillColor2Color=%23f2aa4c&fillColor3Color=%23f2aa4c&fillColor4Color=%23f2aa4c&fillColor5Color=%23f2aa4c&fillColor6Color=%23f2aa4c&fillColor7Color=%23f2aa4c&fillColor8Color=%23f2aa4c&fillColor9Color=%23f2aa4c&fillColor10Color=%23f2aa4c&fillOutlineColor=%23f2aa4c&fillOutline2Color=%23f2aa4c&backgroundColor=%23101820&text='] 
+
+/*============== TEXT ==============*/
+global.wait = '```「▰▰▰▱▱▱▱▱▱▱」Loading...```'
+global.eror = '```404 error```'
+global.dtu = 'ɪɴꜱᴛᴀɢʀᴀᴍ'
+global.dtc = 'ᴄᴀʟʟ ᴏᴡɴᴇʀ'
+global.phn = '+923124442554'
+
+/*=========== TYPE DOCUMENT ===========*/
+global.dpptx = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+global.ddocx = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+global.dxlsx = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+global.dpdf = 'application/pdf'
+global.drtf = 'text/rtf'
+global.djson = 'application/json'
+
+global.thumbdoc = 'https://i.ibb.co/jLnr3K9/20231025-195821.jpg'
+
+/*=========== FAKE SIZE ===========*/
+global.fsizedoc = '5644' // default 10TB
+global.fpagedoc = '999'
+
+/*=========== HIASAN ===========*/
+// DEFAULT MENU
+global.dmenut = 'ଓ═┅═━–〈' //top
+global.dmenub = '┊🇵🇰' //body
+global.dmenub2 = '┊' //body for info cmd on Default menu
+global.dmenuf = '┗––––––––––✦' //footer
+
+// COMMAND MENU
+global.dashmenu = '┅━━━═┅═❏ *Dᴀꜱʜʙᴏᴀʀᴅ* ❏═┅═━━━┅'
+global.cmenut = '❏––––––『'                       //top
+global.cmenuh = '』––––––'                        //header
+global.cmenub = '┊🔖'                            //body
+global.cmenuf = '┗━═┅═━––––––\n'                //footer
+global.cmenua = '\n⌕ ❙❘❙❙❘❙❚❙❘❙❙❚❙❘❙❘❙❚❙❘❙❙❚❙❘❙❙❘❙❚❙❘ ⌕\n     ' //after
+global.pmenus = '┊'                              //pembatas menu selector
+
+global.htki = '––––––『' // Hiasan Titile (KIRI)
+global.htka = '』––––––' // Hiasan Title  (KANAN)
+global.lopr = 'Ⓟ' //LOGO PREMIUM ON MENU.JS
+global.lolm = 'Ⓛ' //LOGO LIMIT/FREE ON MENU.JS
+global.htjava = '⫹⫺'    //hiasan Doang :v
+global.hsquere = ['⛶','❏','⫹⫺']
+
+/*============== STICKER WM ==============*/
+global.stickpack = 'Mr_Malik_'
+global.stickauth = `「 mr-malik 」`
+global.packname = 'Cʀᴇᴀᴛᴇᴅ Bʏ'
+global.packname2 = 'ᴄʀᴇᴀᴛᴇᴅ ʙʏ Nasrullah'
+
+global.multiplier = 38 // The higher, The harder levelup
+
+/*============== EMOJI ==============*/
+global.rpg = {
+  emoticon(string) {
+    string = string.toLowerCase()
+    let emot = {
+      level: '📊',
+      limit: '🎫',
+      health: '❤️',
+      exp: '✨',
+      money: '💹',
+      bank: '🏦',
+      potion: '🥤',
+      diamond: '💎',
+      common: '📦',
+      uncommon: '🛍️',
+      mythic: '🎁',
+      legendary: '🗃️',
+      superior: '💼',
+      pet: '🔖',
+      trash: '🗑',
+      armor: '🥼',
+      sword: '⚔️',
+      pickaxe: '⛏️',
+      fishingrod: '🎣',
+      wood: '🪵',
+      rock: '🪨',
+      string: '🕸️',
+      horse: '🐴',
+      cat: '🐱',
+      dog: '🐶',
+      fox: '🦊',
+      petFood: '🍖',
+      iron: '⛓️',
+      gold: '🪙',
+      emerald: '❇️',
+      upgrader: '🧰'
+      
+    }
+    let results = Object.keys(emot).map(v => [v, new RegExp(v, 'gi')]).filter(v => v[1].test(string))
+    if (!results.length) return ''
+    else return emot[results[0][0]]
+  }
 }
 
-// Sticker WM
-global.botname = 'Mr Malik'
-global.premium = 'true'
-global.packname = ''
-global.author = 'Nasrullah' 
-global.igfg = '' 
-global.dygp = ''
-global.fgsc = '' 
-global.fgyt = ''
-global.fgpyp = ''
-global.fglog = 'https://replicate.delivery/pbxt/Jne2l3QfXx8j2dJwrJ41xmeWE4zTJJfP8UAxNcSEyzzM0MJs/F5qgwI6bMAATgSo.jpeg'
-
-//imagen
-global.imagen1 = fs.readFileSync('./src/Abhi.jpg')
-
-//Dont Edit Danger Zone⚠️
-global.wait = '*⌛ _Please Wait..._*'
-global.rwait = '⌛'
-global.dmoji = '🤭'
-global.done = '✅'
-global.error = '❌' 
-global.mlc = '📃'
-global.sdc = '🎶'
-global.xmoji = '🔥' 
-global.multiplier = 69 
-global.maxwarn = '3' // máxima advertencias
-global.videoList = [];
-global.videoListXXX = [];
-
+//------ JANGAN DIUBAH -----
 let file = fileURLToPath(import.meta.url)
 watchFile(file, () => {
   unwatchFile(file)
   console.log(chalk.redBright("Update 'config.js'"))
   import(`${file}?update=${Date.now()}`)
 })
+
+//------ FUNCTION
+function pickRandom(list) {
+  return list[Math.floor(list.length * Math.random())]
+}
