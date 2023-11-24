7c7402d6d2846159051bba5793aadd9a1aa80205
Nasrullahmalik <130395171+Nasrullahmalik@users.noreply.github.com>
2023-11-24 15:15:17 +0500

    Add files via upload

--- plugins/malik-cricket.js
+++ plugins/malik-cricket.js
@@ -1,71 +1,31 @@
-
 import axios from "axios"
-let handler = async (m, { args }) => {
+ let handler = async (m, { args }) => {
+ if (!args[0]) throw "No matchs found. Or Type correct match id"
+const response = axios.get(`https://cric-theta.vercel.app/score?url=https://m.cricbuzz.com/cricket-commentary/${args}&timestamp=`+new Date());
+const res = await response
 
-try {
-let response = await axios.get(`https://crickbuzz.vercel.app/score?url=https://m.cricbuzz.com/cricket-commentary/75640/ind-vs-nz-1st-semi-final-1st-v-4th-icc-cricket-world-cup-2023`)
-let data = await response.json()
-const title = data.title
-const update = data.update
-const current = data.current
-const batsman = data.batsman
-const batsmanrun = data.batsmanrun
-const strr = data.sr
-const ballsfaced = data.ballsfaced
-const battwo = batsmantwo
-const battworun = data.batsmantworun
-const battwoballsfaced = data.batsmantwoballsfaced
-const battwosr = data.batsmantwosr
-const bowler = data.bowler
-const bover = data.bowlerover
-const brun = data.bowlerruns
-const bwicket = data.bowlerwickets
-const btwo = data.bowlertwo
-const recentb = data.recentballs
-const lastw = data.lastwicket
-const runrate = data.runrate
-const upmd = `${title}\n *${update}*\n\n * ${current}*\nBatsman 🏏: *${batsman} - ${batsmanrun} ${ballsfaced}\n Strike rate: ${strr}\n Batsman 2 🏏: *${battwo}* - ${battworun} ${battwoballsfaced}\n Strike rate: ${battwosr}\n\nBowler ⚾: *${bowler}*\nOver: ${bover}\nRuns: ${brun}\nWickets: ${bwickets}\nBowler 2: ${btwo}\n\n${recentb}\n\nLast wicket ❌ ${lastw}\nRun rate %: *${runrate}*\n`
- m.reply('*Live score updating... 🏏🏏*')
- m.reply(upmd)
- }catch {
-return "*ERROR*"}}
-handler.help = ['.score *<Link>*']
-handler.tags = ['malik']
-handler.command = ['cricket', 'score']
-
+let msg = ''
+if (res.data.title) msg += res.data.title + `\n`
+if (res.data.update) msg += `*`+res.data.update + `*\n\n`
+if (res.data.current) msg += '*'+res.data.current + `*\n`
+if (res.data.batsman) msg += `Batsman 🏏: *${res.data.batsman} - ${res.data.batsmanrun} ${res.data.ballsfaced}\n`
+if (res.data.sr) msg +=`Strike rate: ${res.data.sr}\n`
+if (res.data.batsman) msg +=`Batsman 2 🏏: *${res.data.batsmantwo}* - ${res.data.batsmantworun} ${res.data.batsmantwoballsfaced}\n`
+if (res.data.batsman) msg += `Strike rate: ${res.data.batsmantwosr}\n\n`
+if (res.data.batsman) msg += `Bowler ⚾: *${res.data.bowler}*\n`
+if (res.data.batsman) msg +=`Over: ${res.data.bowlerover}\n`
+if (res.data.batsman) msg += `Runs: ${res.data.bowlerruns}\n`
+if (res.data.batsman) msg +=`Wickets: ${res.data.bowlerwickets}\n`
+if (res.data.batsman) msg+=`Bowler 2: ${res.data.bowlertwo}\n\n`
+if (res.data.batsman) {
+msg += `${res.data.recentballs}\n\n`
+msg += `Last wicket ❌ ${res.data.lastwicket}\n`
+msg += `Run rate %: *${res.data.runrate}*\n`
+}
+m.reply('*Live score updating... 🏏🏏*')
+ m.reply(msg)
+ }
+handler.help = ['score *<matchID>*']
+handler.tags = ['herramientas']
+handler.command = /^(cricket|score)$/i
 export default handler
-
-
-
-//incomplete project
-/*import { getCricketScore, getScoreCard } from "../lib/cricket.js";
-
-let handler = async(m, { conn, command, text, usedPrefix }) => {
-  let { groupDesc, reply } = msgInfoObj;
-  const descErrorMessage = `❌ ERROR
-- Group description is empty.
-- Put match ID in starting of group description. 
-- Get match ID from cricbuzz today match url.
-- example: https://www.cricbuzz.com/live-cricket-scores/37572/mi-vs-kkr-34th-match-indian-premier-league-2021 
-- so match ID is 37572 !
-# If you've put correct match ID in description starting and still facing this error then contact developer wa.me/923470027813\nwa.me/19293514545`;
-
-  if (!groupDesc) {
-    await m.reply(descErrorMessage);
-    return;
-  }
-
-  let matchId = groupDesc.slice(0, 5);
-  if (isNaN(Number(matchId))) {
-    await m.reply(descErrorMessage);
-    return;
-  }
-try {
-  let response = await getCricketScore(matchId);
-  await conn.sendMessage(m.chat, { text: response }, { quoted: m });
-} catch {
-  let response2 = await getScoreCard(matchId);
-  await conn.sendMessage(m.chat, { text: response2 }, { quoted: m });
-}
-}
-handler.command = /^score1$/i*/
