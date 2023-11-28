536434681975f3a1ddf96ad94a73c39d4084e5f4
Nasrullahmalik <130395171+Nasrullahmalik@users.noreply.github.com>
2023-11-28 16:45:39 +0500

    Add files via upload

--- plugins/dl-playlist.js
+++ plugins/dl-playlist.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn, groupMetadata, usedPrefix, text, args, command }
 `
 let buttons = [{ buttonText: { displayText: 'Video' }, buttonId: `${usedPrefix}video ${text} ` }, { buttonText: { displayText: 'Audio' }, buttonId: `${usedPrefix}playdoc ${text} ` }]
 await m.react('🔁')						 
-let msg = await conn.sendMessage(m.chat, { image: { url: 'https://i.ibb.co/XJ6gsgV/IMG-20231117-WA0020.jpg' }, caption: capt, footer: `${footerTXT}`, buttons }, { quoted: m })
+conn.sendMessage(m.chat, { image: { url: 'https://i.ibb.co/XJ6gsgV/IMG-20231117-WA0020.jpg' }, caption: capt, footer: `${footerTXT}`, buttons }, { quoted: m })
 }
 
 handler.command = /^playlist?$/i
