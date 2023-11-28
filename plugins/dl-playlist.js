37ba5ee127a16d9df3b1a997b7a56526ff9d3224
Nasrullahmalik <130395171+Nasrullahmalik@users.noreply.github.com>
2023-11-28 17:21:51 +0500

    Add files via upload

--- plugins/dl-playlist.js
+++ plugins/dl-playlist.js
@@ -1,16 +1,42 @@
-import fetch from 'node-fetch'
-let handler = async (m, { conn, groupMetadata, usedPrefix, text, args, command }) => {
- let capt = `*${htki} YOUTUBE ${htka}*
-ᴘʟᴇᴀsᴇ sᴇʟᴇᴄᴛ ᴛʜᴇ ᴛʏᴘᴇ ʙᴇʟᴏᴡ
-• ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ ᴘʟᴀʏᴀʙʟᴇ ᴡʜᴀᴛsᴀᴘᴘ ᴠɪᴅᴇᴏ ʜɪᴛ ᴠɪᴅᴇᴏ ʙᴜᴛᴛᴏɴ
-• ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ ᴘʟᴀʏᴀʙʟᴇ ᴡʜᴀᴛsᴀᴘᴘ Audio ʜɪᴛ Audio ʙᴜᴛᴛᴏɴ
-`
-let buttons = [{ buttonText: { displayText: 'Video' }, buttonId: `${usedPrefix}video ${text} ` }, { buttonText: { displayText: 'Audio' }, buttonId: `${usedPrefix}playdoc ${text} ` }]
-await m.react('🔁')						 
-conn.sendMessage(m.chat, { image: { url: 'https://i.ibb.co/XJ6gsgV/IMG-20231117-WA0020.jpg' }, caption: capt, footer: `${footerTXT}`, buttons }, { quoted: m })
-}
+import yts from 'yt-search';
+import fs from 'fs';
+const handler = async (m, {conn, text, usedPrefix, command}) => {
+  if (!text) throw `*[❗] Name/title is missing*\n\n*—◉ Example:*\n*${usedPrefix + command} Begin you*`;
+  try {
+    const vids_ = {
+      from: m.sender,
+      urls: [],
+    };
+    if (!global.videoList) {
+      global.videoList = [];
+    }
+    if (global.videoList[0]?.from == m.sender) {
+      global.videoList.splice(0, global.videoList.length);
+    }
+    const results = await yts(text);
+    const textoInfo = `*[❗] Puedes descargar el video que quieras de la siguiente forma:*
+◉ ${usedPrefix}play <number>
+◉ ${usedPrefix}video <number> 
 
-handler.command = /^playlist?$/i
-
-
-export default handler
+*—◉ Ejemplos:*
+*◉ ${usedPrefix}play 5*
+*◉ ${usedPrefix}video 8*`.trim();
+    const teks = results.all.map((v, i) => {
+      const link = v.url;
+      vids_.urls.push(link);
+      return `[${i + 1}] ${v.title}
+↳ 🫐 *_Link :_* ${v.url}
+↳ 🕒 *_Duration :_* ${v.timestamp}
+↳ 📥 *_Uploaded :_* ${v.ago}
+↳ 👁 *_Views :_* ${v.views}`;
+    }).join('\n\n◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦\n\n');
+    conn.sendFile(m.chat, results.all[0].thumbnail, 'yts.jpeg', textoInfo + '\n\n' + teks, m);
+    global.videoList.push(vids_);
+  } catch {
+    await m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾 𝙲𝙾𝙽 𝙾𝚃𝚁𝙾 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝚄𝙽𝙰 𝙲𝙰𝙽𝙲𝙸𝙾𝙽*');
+  }
+};
+handler.help = ['playlist *<texto>*'];
+handler.tags = ['search'];
+handler.command = /^playlist|playlist2$/i;
+export default handler;
